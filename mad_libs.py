c9cb602d01aff6519041f7eb7e5da0d944df5554
jphan344 <jenniferphan202@gmail.com>
2024-11-04 15:58:03 -0500

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,19 @@
+print("Hello! Welcome to Animal Crossing Mad Libs. Can you finish this story?")
+
+town_name = input("town name:")
+
+animal = input ("animal:")
+
+name = input ("name:")
+
+adjective = input ("adjective:")
+
+place = input ("place:")
+
+noun = input ("noun:")
+
+award_name = input ("award name:")
+
+verb = input ("present tense verb:")
+
+print("Once upon a time in " + town_name + ", a " + animal + " named " + name + " lived in a " + adjective + " town. One day " + name + " walked into the " + place + " and saw a " + noun + ". Determined to win the " + award_name + " award, " + name + " began to " + verb + " every " + noun + " in sight!")
